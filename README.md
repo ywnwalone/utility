18fe6fa15b206ac5408f8e550121c7a10acb7773
1048576 <ywnwalone@naver.com>
2017-12-07 14:27:02 +0900

    add isHex(), Hex2Dec()

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 
 
-# pad0
+## pad0()
+
+> 문자열 앞에 0 또는 원하는 문자를 원하는 갯수만큼 붙여주는 함수입니다.
 
 ```javascript
 String.prototype.pad0 = function(len, c){
@@ -11,5 +13,31 @@ String.prototype.pad0 = function(len, c){
         s = c + s;
     }
     return s;
+}
+```
+
+## isHex()
+
+> 문자열이 16진수인지 판별하는 함수입니다.
+
+```js
+String.prototype.isHex = function(){
+    var s = this, regex = /[0-9A-Fa-f]{6}/g;
+    if(regex.test(s)){
+        return true;
+    }else{
+        return false;
+    }
+}
+```
+
+## Hex2Dec()
+
+> 16진수 문자열을 10진수 문자열로 변환하는 함수입니다.
+
+```js
+String.prototype.Hex2Dec = function(){
+    var s = this;
+    return parseInt(s, 16);
 }
 ```
--- utility.js
+++ utility.js
@@ -9,3 +9,44 @@ String.prototype.pad0 = function(len, c){
     }
     return s;
 }
+
+
+/*
+    RGB => HEX || HEX => RGB
+*/
+String.prototype.convertColorFormat = function(){
+    var s = this;
+    if(s[0] == '#'){
+        if(s.length != 7){
+            console.error('Hex color must be composed 6 characters... ex)#FFFAAA ');
+        }else{
+            //s.substring(1,7) 이 hex인지 판별
+            var hex = s.substring(1,7);
+            if(!hex.isHex()){
+                console.error(`${s} is not hexadecimal...`)
+            }else{
+                return `rgb(${hex.substring(0,2).Hex2Dec()}, ${hex.substring(2,4).Hex2Dec()}, ${hex.substring(4,6).Hex2Dec()})`;
+            }
+        }
+    }else if(s.substring(0,3) == 'rgb' || s.substring(0,3) == 'RGB'){
+        // length로 판별 불가. 
+        if(s[4] != '(' || s[s.length] != ')'){
+            console.error('It is not RGB format..');
+        }
+    }else{
+        console.error('Check your String...');
+    }
+}
+String.prototype.isHex = function(){
+    var s = this, regex = /[0-9A-Fa-f]{6}/g;
+    if(regex.test(s)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+String.prototype.Hex2Dec = function(){
+    var s = this;
+    return parseInt(s, 16);
+}
